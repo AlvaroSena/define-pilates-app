f4a57ac734612655a9b32ca7e9a449e86c9059d4
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-09-24 20:43:39 -0300

    chore: add react as the views layer of MVC and setup monorepo

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Define pilates app
--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,6 +1,13 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.WEB_ORIGIN!,
+    methods: "*",
+  })
+);
 app.use(express.json());
 
 app.get("/", (request, response) => {
